a0769f32e995fc00e6fb3194eb218a0f4419d164
Kah Soon Yap <kahsoonyap1998@hotmail.com>
2019-02-11 10:11:30 -0500

    delims

---
+++ delim.js
@@ -0,0 +1,32 @@
+var delim = function(str) {
+  const delims = {
+    '(' : ')',
+    '[' : ']',
+    '{' : '}',
+    '\'' : '\'',
+    '\"' : '\"'
+  };
+  let stack = [];
+  let ele = '';
+  for (i = 0; i < str.length; i++) {
+    if (str[i] in delims) {
+      stack.push(str[i]);
+    } else if (Object.keys(delims).find(key => delims[key] === str[i]) !== undefined) {
+      ele = stack.pop();
+      if (delims[ele] != str[i]) {
+        return false;
+      }
+    }
+  }
+  if (stack.length == 0) {
+    return true;
+  }
+  return false;
+}
+
+console.log(delim('(123[456]{7})'));
+console.log(delim('{ab(c[d[e]])}'));
+console.log(delim('78[()]{}'));
+console.log(delim('(12[3)]'));
+console.log(delim('{abc[de]'));
+console.log(delim('({[})]'));
